06dc8384ef47d2888759c0e387ca918f043cf1ff
aa.ibragimov <artem.ibragimov@gmail.com>
2020-05-01 09:38:26 +0300

    max_profit
    https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/

--- src/arrays/max_profit.rs
+++ src/arrays/max_profit.rs
@@ -1,70 +1,12 @@
 fn max_profit(prices: Vec<i32>) -> i32 {
-   if prices.is_empty() {
-      return 0;
-   }
+   if prices.is_empty() { return 0; }
    let mut prev_price: i32 = *prices.first().unwrap();
-   let dynamic: Vec<i32> = prices
-      .into_iter()
-      .map(|price| {
-         let d = price - prev_price;
-         prev_price = price;
-         d
-      })
-      .collect();
-   println!("dynamic {:?}", dynamic);
-   let (price_to_buy, sum) = max_vec(dynamic);
-   sum - price_to_buy
-}
-
-fn max_vec(data: Vec<i32>) -> (i32, i32) {
-   use std::cmp::max;
-   use std::iter::FromIterator;
-   let len = data.len();
-   if len <= 1 {
-      return match len {
-         0 => (0, 0),
-         _ => (data[0], data[0]),
-      };
+   let mut profit: i32 = 0;
+   for price in prices {
+      if price - prev_price > 0 { profit += price - prev_price; }
+      prev_price = price;
    }
-   println!("data {:?}", data);
-
-   let mut mid_price: i32 = 0;
-   let mut mid_sum: i32 = 0;
-   for price in data[0..len / 2].iter().rev() {
-      println!("compare sum {} & price {}", mid_sum, price);
-      if *price == 0 {
-         break;
-      }
-      if mid_sum + price < mid_sum {
-         mid_price = *price;
-         break;
-      }
-      mid_price = *price;
-      mid_sum += price;
-   }
-   for price in data[len / 2..].iter() {
-      println!("compare sum {} & price {}", mid_sum, price);
-      if mid_sum + price <= mid_sum {
-         break;
-      }
-      mid_sum += price;
-   }
-
-   let (left_price, left_sum) = max_vec(Vec::from_iter(data[..len / 2].iter().cloned()));
-   print!("left {:?},  ", (left_price, left_sum));
-
-   print!("mid {:?},  ", (mid_price, mid_sum));
-
-   let (right_price, right_sum) = max_vec(Vec::from_iter(data[len / 2..].iter().cloned()));
-   println!("right {:?}", (right_price, right_sum));
-   let max_sum = max(mid_sum, max(left_sum, right_sum));
-   if max_sum == left_sum {
-      return (left_price, left_sum);
-   }
-   if max_sum == right_sum {
-      return (right_price, right_sum);
-   }
-   (mid_price, mid_sum)
+   profit
 }
 
 #[cfg(test)]
